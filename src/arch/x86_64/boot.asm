46bf2ffbb9a20fbec0e52c81ff6b293d1da8723d
holmgr <viktor.holmgren@gmail.com>
2016-04-15 19:32:18 +0000

    Implement check for Multiboot, CPUID and Long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -4,9 +4,79 @@ section .text
 bits 32
 start:
 	mov esp, stack_top
+
+	call check_multiboot
+	call check_cpuid
+	call check_long_mode
+
 	; print 'OK' to screen
 	mov dword [0xb8000], 0x2f4b2f4f
 	hlt
+
+; Throw error '0' if eax does not contain Multiboot 2 magic value
+check_multiboot:
+	cmp eax, 0x36d76289
+	jne .no_multiboot
+	ret
+.no_multiboot:
+	mov al, "0"
+	jmp error
+
+; Throw error '1' if the CPU does not support CPUID command
+check_cpuid:
+	; Check if CPUID is supported by attempting to flip the ID bit (bit 21) in
+    ; the FLAGS register. If we can flip it, CPUID is available.
+
+    ; Copy FLAGS in to EAX via stack
+    pushfd
+    pop eax
+
+    ; Copy to ECX as well for comparing later on
+    mov ecx, eax
+
+    ; Flip the ID bit
+    xor eax, 1 << 21
+
+    ; Copy EAX to FLAGS via the stack
+    push eax
+    popfd
+
+    ; Copy FLAGS back to EAX (with flipped bit if CPUID supported)
+    pushfd
+    pop eax
+
+    ; Restore FLAGS from the old version stored in ECX (flipping the ID back
+    ; if it was flipped before)
+    push ecx
+    popfd
+
+    ; Compare EAX and ECX, if equal then CPUID is not supported
+    cmp eax, ecx
+    je .no_cpuid
+    ret
+
+.no_cpuid:
+	mov al, "1"
+	jmp error
+
+; Test if extended processor info is available
+check_long_mode:
+	mov eax, 0x80000000		; implicit argument for cpuid
+	cpuid					; get highest supported arg
+	cmp eax, 0x80000001		; it needs to be at least this
+	jb .no_long_mode		; if less, CPU too old for long mode
+
+	; use extended info to test if long mode is available
+	mov eax, 0x80000001		; argument of extended processor info
+	cpuid					; return various feature bits in ecx and edx
+	test edx, 1 << 29		; test if the LM-bit is set in the D-register
+	jz .no_long_mode		; if not set, there is no long mode
+	ret
+.no_long_mode:
+	mov al, "2"
+	jmp error
+
+
 
 ; Prints 'ERR: ' and the given error code to screen and hangs
 ; parameter: error code (in ascii) in al
