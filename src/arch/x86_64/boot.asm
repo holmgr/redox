f0738f55ec8ccd087cb2b45e42c7c03ec13d3a36
holmgr <viktor.holmgren@gmail.com>
2016-04-15 21:11:22 +0000

    Setup SSE as rust needs it

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -12,6 +12,7 @@ start:
 
 	call set_up_page_tables
 	call enable_paging
+	call set_up_SSE
 
 	; load the 64-bit GDT
 	lgdt [gdt64.pointer]
@@ -136,6 +137,28 @@ enable_paging:
 	mov cr0, eax
 
 	ret
+
+; Check for SSE and enable it. If it's not supported throw error "a"
+set_up_SSE:
+	; check for SSE
+	mov eax, 0x1
+	cpuid
+	test edx, 1<<25
+	jz .no_SSE
+
+	; enable SSE
+	mov eax, cr0
+	and ax, 0xFFFB	; clear coprocessor emulation CR0.EM
+	or ax, 0x2			; set coprocessor monitoring CR0.MP
+	mov cr0, eax
+	mov eax, cr4
+	or ax, 3 << 9		; set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+	mov cr4, eax
+
+	ret
+.no_SSE:
+	mov al, "a"
+	jmp error
 
 ; Prints 'ERR: ' and the given error code to screen and hangs
 ; parameter: error code (in ascii) in al
