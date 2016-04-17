306a9c46a0383e9cd18b82dba82414f78db13998
holmgr <viktor.holmgren@gmail.com>
2016-04-17 21:47:00 +0000

    Add simple allocator test in main, allocate all frames

--- src/lib.rs
+++ src/lib.rs
@@ -44,6 +44,17 @@ pub extern fn rust_main(multiboot_information_address: usize) {
 	println!("kernel_start: 0x{:x}, kernel_end: 0x{:x}", kernel_start, kernel_end);
 	println!("multiboot_start: 0x{:x}, multiboot_end: 0x{:x}", multiboot_start, multiboot_end);
 
+	let mut frame_allocator = memory::AreaFrameAllocator::new(
+		kernel_start as usize, kernel_end as usize, multiboot_start,
+		multiboot_end, memory_map_tag.memory_areas());
+
+	for i in 0.. {
+		if let None = frame_allocator.allocate_frame() {
+			println!("allocated {} frames", i);
+			break;
+		}
+	}
+
 	loop{}
 }
 
