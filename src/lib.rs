0268c0bab964da1596669160687c7b5a1854b691
holmgr <viktor.holmgren@gmail.com>
2016-04-17 20:45:22 +0000

    Print kernel elf sections in main

--- src/lib.rs
+++ src/lib.rs
@@ -11,25 +11,34 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main(multiboot_information_address: usize) {
-    vga_buffer::clear_screen();
-    println!("Hello World{}", "!");
+	vga_buffer::clear_screen();
+	println!("Hello World{}", "!");
 
-    let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
-    let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
+	let boot_info = unsafe { multiboot2::load(multiboot_information_address) };
+	let memory_map_tag = boot_info.memory_map_tag().expect("Memory map tag required");
 
-    println!("memory areas:");
-    for area in memory_map_tag.memory_areas() {
-        println!("  start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
-    }
+	println!("memory areas:");
+	for area in memory_map_tag.memory_areas() {
+		println!("  start: 0x{:x}, length: 0x{:x}", area.base_addr, area.length);
+	}
 
-    loop{}
+	let elf_sections_tag = boot_info.elf_sections_tag()
+		.expect("Elf-sections tag required");
+
+	println!("kernel sections");
+	for section in elf_sections_tag.sections() {
+		println!("  addr: 0x{:x}, size: 0x{:x}, flags: 0x{:x}",
+			section.addr, section.size, section.flags);
+	}
+
+	loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 
 #[lang = "panic_fmt"]
 extern fn panic_fmt(fmt: core::fmt::Arguments, file: &str, line: u32) -> ! {
-    println!("\n\nPanic in {} at line {}:", file, line);
-    println!("  {}", fmt);
-    loop {}
+	println!("\n\nPanic in {} at line {}:", file, line);
+	println!("  {}", fmt);
+	loop {}
 }
